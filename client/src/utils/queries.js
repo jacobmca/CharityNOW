31534f4c8aeda6275cd6e44dc8ee3179c8bebff2
Mehrdad Rezvany <mrezvany@outlook.com>
2024-09-17 19:08:21 -0700

    Reimplemented queries.js and mutations.js on the client side

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,35 +1,69 @@
 import { gql } from '@apollo/client';
-export const QUERY_DONATIONS = gql`
-    query donations {
-        donations {
-            _id
-            title
-            description
-            amount
-            date
-        }
-    }
-`;
+
 export const GET_ME = gql`
   query me {
     me {
       _id
       username
       email
+      charities {
+        _id
+        name
+        description
+        address
+        image
+        amountContributed
+      }
       donations {
         _id
         amount
-        createdAt
-        message
+        charity {
+          _id
+          name
+        }
       }
     }
   }
 `;
-export const QUERY_ORGANIZATIONS = gql`
-  query getOrganizations {
-    organizations {
+
+export const GET_ALL_CHARITIES = gql`
+  query allCharities {
+    allCharities {
       _id
       name
+      description
+      address
+      image
+      amountContributed
+      users {
+        _id
+        username
+      }
+      donations {
+        _id
+        amount
+      }
+    }
+  }
+`;
+
+export const GET_CHARITY = gql`
+  query charity($id: ID!) {
+    charity(_id: $id) {
+      _id
+      name
+      description
+      address
+      image
+      amountContributed
+      users {
+        _id
+        username
+      }
+      donations {
+        _id
+        amount
+      }
     }
   }
 `;
